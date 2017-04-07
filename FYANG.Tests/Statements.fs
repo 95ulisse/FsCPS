3c03ea3a41c2df2dc83f0d4ca6150466886b66d0
Marco Cameriero <me@marcocameriero.net>
2017-04-07 16:30:02 +0200

    Switched to .NET Framework 4.6.1. .Net Core support will be added later.

--- FYANG.Tests/Statements.fs
+++ FYANG.Tests/Statements.fs
@@ -20,15 +20,6 @@ module Identifiers =
     let ``Identifiers cannot start with a digit`` () =
         AssertFailure id "123"
 
-    [<Fact>]
-    let ``Identifiers cannot start with X, M or L`` () =
-        AssertFailure id "X123"
-        AssertFailure id "x123"
-        AssertFailure id "M123"
-        AssertFailure id "m123"
-        AssertFailure id "L123"
-        AssertFailure id "l123"
-
 
 
 module Strings =
