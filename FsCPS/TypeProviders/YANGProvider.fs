32ce736239f5055089375f6332c75820459891be
Marco Cameriero <me@marcocameriero.net>
2017-06-30 17:18:24 +0200

    Implemented generation for container nodes.

--- FsCPS/TypeProviders/YANGProvider.fs
+++ FsCPS/TypeProviders/YANGProvider.fs
@@ -11,6 +11,7 @@ open Microsoft.FSharp.Quotations
 open Microsoft.FSharp.Quotations.Patterns
 open ProviderImplementation
 open ProviderImplementation.ProvidedTypes
+open ProviderImplementation.ProvidedTypes.UncheckedQuotations
 open FsCPS
 open FsCPS.Yang
 open FsCPS.TypeProviders.Runtime.YANGProviderRuntime
@@ -43,6 +44,9 @@ type internal YANGProviderGenerationContext() =
 
     member this.CurrentPath
         with get() = pathStack.Peek()
+        
+    member this.IsEmpty
+        with get() = nodeStack.Count = 0
 
 
 [<TypeProvider>]
@@ -75,7 +79,7 @@ type YANGProvider(config: TypeProviderConfig) as this =
     // Returns the actual type to use for the given YANG type.
     // Tries to use pritive types as far as it can, otherwise (like in the case
     // of enums and unions) generates a new type.
-    let generateType (ctx: YANGProviderGenerationContext) (yangType: YANGType) =
+    let generateLeafType (ctx: YANGProviderGenerationContext) (yangType: YANGType) =
         match yangType.PrimitiveType with
         | x when x = YANGPrimitiveTypes.Empty       -> failwith "Invalid type Empty."
         | x when x = YANGPrimitiveTypes.Boolean     -> typeof<bool>
@@ -94,12 +98,35 @@ type YANGProvider(config: TypeProviderConfig) as this =
         | x when x = YANGPrimitiveTypes.Union       -> failwith "Unions not implemented."
         | _ ->
             failwithf "Unexpected primitive type %A." yangType.PrimitiveType.Name
+            
+    /// Generetes a new type for container nodes.
+    let rec generateContainerType (ctx: YANGProviderGenerationContext) (container: YANGContainer) =
+        
+        // Crate a new type with the same name of the container and add it to the parent type
+        let newType =
+            factory.ProvidedTypeDefinition(
+                normalizeName container.Name.Name,
+                Some typeof<CPSObject>,
+                hideObjectMethods = true,
+                nonNullable = true
+            )
+            |> generateCommonMembers
+        ctx.CurrentType.AddMember(newType)
+        
+        // Recursively generate the types 
+        ctx.Push(container, newType, ctx.CurrentPath.Append(container.Name.Name))
+        container.DataNodes |> Seq.iter (generateTypesForNode ctx)
+        ctx.Pop()
+        
+        newType
+        
 
-    // Adds the common node constructors to the given type:
+    // Adds the common node members to the given type:
     // - A constructor accepting a CPSObject to use the given object as a backing store.
     // - A constructor requiring a CPSKey to create a backing CPSObject.
     // - Some other overloads for the last constructor.
-    let generateCommonConstructors (t: ProvidedTypeDefinition) =
+    // - A CPSObject property to extract the underlying CPSObject.
+    and generateCommonMembers (t: ProvidedTypeDefinition) =
         let ctor1 =
             factory.ProvidedConstructor(
                 [ factory.ProvidedParameter("obj", typeof<CPSObject>) ],
@@ -128,12 +155,23 @@ type YANGProvider(config: TypeProviderConfig) as this =
                 ],
                 (fun args -> <@@ CPSObject((%%(args.[0]) : CPSPath), (%%(args.[1]) : CPSQualifier)) @@>)
             )
+        
+        let objProp =
+            factory.ProvidedMethod(
+                "ToCPSObject",
+                [],
+                typeof<CPSObject>,
+                (fun args -> <@@ %%(args.[0]) : CPSObject @@>),
+                isStatic = false
+            )
 
         t.AddMembers([ ctor1; ctor2; ctor3; ctor4; ctor5 ])
+        t.AddMember(objProp)
         t
+        
 
     // Common getter for leaf data nodes.
-    let leafPropertyGetter t path (args: Quotations.Expr list) =
+    and leafPropertyGetter t path (args: Quotations.Expr list) =
         let expr = <@@ readAttribute<obj> (CPSPath path) (%%(args.[0]) : CPSObject) @@>
         
         // Be sure that we call the `readAttribute` method with the correct generic parameter
@@ -144,7 +182,7 @@ type YANGProvider(config: TypeProviderConfig) as this =
             failwith "Should never be reached"
 
     // Common setter for leaf data nodes.
-    let leafPropertySetter t path (args: Quotations.Expr list) =
+    and leafPropertySetter t path (args: Quotations.Expr list) =
         let expr = <@@ writeAttribute<obj> (CPSPath path) None (%%(args.[0]) : CPSObject) @@>
         
         // Be sure that we call the `writeAttribute` method with the correct generic parameter
@@ -153,17 +191,32 @@ type YANGProvider(config: TypeProviderConfig) as this =
             Expr.Call(mtd.GetGenericMethodDefinition().MakeGenericMethod([| t |]), [ arg1; args.[1]; arg3 ])
         | _ ->
             failwith "Should never be reached"
+            
+    // Common getter for container nodes
+    and containerPropertyGetter (t: ProvidedTypeDefinition) (args: Quotations.Expr list) =
+        // This is a manual construction for:
+        // <@@ new t(args.[0] : CPSObject) @@>
+        let ctor =
+            t.GetMembers(BindingFlags.Instance ||| BindingFlags.Public)
+            |> Array.find (fun m ->
+                m.MemberType = MemberTypes.Constructor && (
+                    let pars = (m :?> ConstructorInfo).GetParameters()
+                    pars.Length = 1 && pars.[0].ParameterType.Name.Contains(typeof<CPSObject>.Name)
+                ) 
+            )
+        Expr.NewObjectUnchecked(ctor :?> ConstructorInfo, [ args.[0] ])
+
 
     // Adds new members to the given parent type from the given data node.
     // Adds new nested types for containers, lists and leaf-lists. For the leafs,
     // corresponding instance properties are also added.
-    let generateTypes (ctx: YANGProviderGenerationContext) (node: YANGDataNode) =
+    and generateTypesForNode (ctx: YANGProviderGenerationContext) (node: YANGDataNode) =
         match node with
 
         | :? YANGLeaf as leaf ->
             
             // Generates the type for the leaf
-            let leafType = generateType ctx leaf.Type.ResolvedType
+            let leafType = generateLeafType ctx leaf.Type.ResolvedType
 
             // Adds an instance property to the parent type for the leaf
             let prop =
@@ -178,7 +231,21 @@ type YANGProvider(config: TypeProviderConfig) as this =
             ctx.CurrentType.AddMember(prop)
         
         | :? YANGContainer as container ->
-            failwith "YANGContainer not implemented."
+            
+            // Generates a container type
+            let containerType = generateContainerType ctx container
+            
+            // Adds an instance property to the parent type for the container
+            let prop =
+                factory.ProvidedProperty(
+                    normalizeName container.Name.Name,
+                    containerType,
+                    containerPropertyGetter containerType
+                )
+            if not (isNull container.Description) then
+                prop.AddXmlDoc(container.Description)
+            ctx.CurrentType.AddMember(prop)
+            
         
         | :? YANGList as list ->
             failwith "YANGList not implemented."
@@ -188,20 +255,26 @@ type YANGProvider(config: TypeProviderConfig) as this =
         
         | _ -> ()
 
+
     // Generates the root type for a module, and recursively generates all the types for the inner nodes
     let generateRootTypeForModule typeName rootPath (m: YANGModule) =
         
         // Generates the root type
         let rootType =
             factory.ProvidedTypeDefinition(asm, ns, typeName, Some typeof<CPSObject>, hideObjectMethods = true, nonNullable = true)
-            |> generateCommonConstructors
+            |> generateCommonMembers
 
         // Creates a new generation context
         let ctx = YANGProviderGenerationContext()
         ctx.Push(m, rootType, CPSPath(rootPath))
 
         // Generates the subtypes from the other data nodes
-        m.DataNodes |> Seq.iter (generateTypes ctx)
+        m.DataNodes |> Seq.iter (generateTypesForNode ctx)
+        
+        // Assert correct stack balance
+        ctx.Pop()
+        if not ctx.IsEmpty then
+            failwith "YANGProviderGenerationContext is not balanced"
 
         Testing.FormatProvidedType(rootType, signatureOnly = false, ignoreOutput = false, useQualifiedNames = false)
         |> printf "%s\n"
--- FsCPS/TypeProviders/YANGProviderRuntime.fs
+++ FsCPS/TypeProviders/YANGProviderRuntime.fs
@@ -11,55 +11,42 @@ open FsCPS
 /// Internals of the runtime for the YANGProvider.
 module YANGProviderRuntime =
 
-    let private dataReaders = Dictionary<Type, byte[] -> obj>()
-    let private dataWriters = Dictionary<Type, obj -> byte[]>()
+    let private transformers = Dictionary<Type, (byte[] -> obj) * (obj -> byte[])>()
 
-    // Common data readers
+    // Transformers for common types
     do
-        let wrap f x = box (f(x, 0))
-        dataReaders.Add(typeof<bool>, wrap BitConverter.ToBoolean)
-        dataReaders.Add(typeof<int8>, (fun arr -> box (int8 arr.[0])))
-        dataReaders.Add(typeof<int16>, wrap BitConverter.ToInt16)
-        dataReaders.Add(typeof<int32>, wrap BitConverter.ToInt32)
-        dataReaders.Add(typeof<int64>, wrap BitConverter.ToInt64)
-        dataReaders.Add(typeof<uint8>, (fun arr -> box (uint8 arr.[0])))
-        dataReaders.Add(typeof<uint16>, wrap BitConverter.ToUInt16)
-        dataReaders.Add(typeof<uint32>, wrap BitConverter.ToUInt32)
-        dataReaders.Add(typeof<uint64>, wrap BitConverter.ToUInt64)
-        dataReaders.Add(typeof<byte[]>, box)
-        dataReaders.Add(typeof<string>, Encoding.UTF8.GetString >> box)
-        dataReaders.Add(typeof<double>, (fun arr -> BitConverter.Int64BitsToDouble(BitConverter.ToInt64(arr, 0)) |> box))
-        
-    // Common data writers
-    do
-        let inline add (f: ^a -> byte[]) = dataWriters.Add(typeof< ^a >, unbox< ^a > >> f)
-        add<bool> BitConverter.GetBytes
-        add<int8> (fun x -> [| byte x |])
-        add<int16> BitConverter.GetBytes
-        add<int32> BitConverter.GetBytes
-        add<int64> BitConverter.GetBytes
-        add<uint8> (fun x -> [| byte x |])
-        add<uint16> BitConverter.GetBytes
-        add<uint32> BitConverter.GetBytes
-        add<uint64> BitConverter.GetBytes
-        add<byte[]> id
-        add<string> Encoding.UTF8.GetBytes
-        add<double> (BitConverter.DoubleToInt64Bits >> BitConverter.GetBytes)
-        
+        let inline wrapr f x = f(x, 0) |> box
+        let inline wrapw (f: ^a -> byte[]) = unbox< ^a > >> f
+        let inline add (reader: byte[] * int -> ^a) (writer: ^a -> byte[]) =
+            transformers.Add(typeof< ^a >, (wrapr reader, wrapw writer))
+
+        add<bool> BitConverter.ToBoolean BitConverter.GetBytes
+        add<int8> (fun (arr, _) -> int8 arr.[0]) (fun x -> [| byte x |])
+        add<int16> BitConverter.ToInt16 BitConverter.GetBytes
+        add<int32> BitConverter.ToInt32 BitConverter.GetBytes
+        add<int64> BitConverter.ToInt64 BitConverter.GetBytes
+        add<uint8> (fun (arr, _) -> uint8 arr.[0]) (fun x -> [| byte x |])
+        add<uint16> BitConverter.ToUInt16 BitConverter.GetBytes
+        add<uint32> BitConverter.ToUInt32 BitConverter.GetBytes
+        add<uint64> BitConverter.ToUInt64 BitConverter.GetBytes
+        add<byte[]> (fun (arr, _) -> arr) id
+        add<string> (fun (arr, _) -> Encoding.UTF8.GetString arr) Encoding.UTF8.GetBytes
+        add<double> (fun (arr, _) -> BitConverter.Int64BitsToDouble(BitConverter.ToInt64(arr, 0)))
+                    (BitConverter.DoubleToInt64Bits >> BitConverter.GetBytes)
+       
 
     /// Registers a new type along with its reader and writer.
     let registerType<'a> (reader: byte[] -> obj) (writer: obj -> byte[]) =
-        if dataReaders.ContainsKey(typeof<'a>) then
+        if transformers.ContainsKey(typeof<'a>) then
             invalidOp (sprintf "Type %s has already been registered." typeof<'a>.Name)
-        dataReaders.Add(typeof<'a>, reader)
-        dataWriters.Add(typeof<'a>, writer)
+        transformers.Add(typeof<'a>, (reader, writer))
         
             
     [<EditorBrowsableAttribute(EditorBrowsableState.Never)>]
     [<CompilerMessageAttribute("This method is intended for use in generated code only.", 10001, IsHidden=true, IsError=false)>]
     let readAttribute<'a> (path: CPSPath) (obj: CPSObject) : 'a option =
-        match dataReaders.TryGetValue(typeof<'a>) with
-        | (true, reader) ->
+        match transformers.TryGetValue(typeof<'a>) with
+        | (true, (reader, _)) ->
             obj.GetAttribute(path)
             |> Option.map (fun attr -> reader attr.Value :?> 'a)
         | _ ->
@@ -69,8 +56,8 @@ module YANGProviderRuntime =
     [<EditorBrowsableAttribute(EditorBrowsableState.Never)>]
     [<CompilerMessageAttribute("This method is intended for use in generated code only.", 10001, IsHidden=true, IsError=false)>]
     let writeAttribute<'a> (path: CPSPath) (attrVal: 'a option) (obj: CPSObject) =
-        match dataWriters.TryGetValue(typeof<'a>) with
-        | (true, writer) ->
+        match transformers.TryGetValue(typeof<'a>) with
+        | (true, (_, writer)) ->
             match attrVal with
             | Some v -> obj.SetAttribute(path, writer (box v))
             | None -> obj.RemoveAttribute(path) |> ignore
