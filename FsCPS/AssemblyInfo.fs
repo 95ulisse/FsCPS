e58d729477158db32b6edea5d58bb2323bc855f9
Marco Cameriero <me@marcocameriero.net>
2017-08-03 16:14:06 +0200

    Set version to 0.1

--- FsCPS/AssemblyInfo.fs
+++ FsCPS/AssemblyInfo.fs
@@ -35,8 +35,8 @@ open Microsoft.FSharp.Core.CompilerServices
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyFileVersion("1.0.0.0")>]
+[<assembly: AssemblyVersion("0.1.0.0")>]
+[<assembly: AssemblyFileVersion("0.1.0.0")>]
 
 // Internals must be visible to the tests.
 [<assembly: InternalsVisibleTo("FsCPS.Tests")>]
